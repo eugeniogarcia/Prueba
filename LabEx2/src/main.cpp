8bcb769c080fff1536a74c142c41fb7c1a9b2f72
Eugenio Garcia San Martin <egsmartin@gmail.com>
2024-11-23 21:38:54 +0100

    ejercio2

--- LabEx2/src/main.cpp
+++ LabEx2/src/main.cpp
@@ -1,42 +1,27 @@
 #include <Arduino.h>
 
 char texto[100];
+void function(int *a, int b);
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Introduction to pointers \n");
+  Serial.println("Function parameters\n");
 }
 
 void loop()
 {
-  int *pc, c;
-
-  c = 22;
-  sprintf(texto, "Address of c: %p\n", &c);
+  int *pa, a, b;
+  a = 3;
+  b = 2;
+  pa = &a;
+  function(pa, b);
+  sprintf(texto, "a=%d b=%d", a, b);
   Serial.println(texto);
+}
 
-  sprintf(texto, "Value of c: %d\n\n", c); // 22
-  Serial.println(texto);
-
-  pc = &c;
-  sprintf(texto, "Address of pointer pc: %p\n", pc);
-  Serial.println(texto);
-
-  sprintf(texto, "Content of pointer pc: %d\n\n", *pc); // 22
-  Serial.println(texto);
-
-  c = 11;
-  sprintf(texto, "Address of pointer pc: %p\n", pc);
-  Serial.println(texto);
-
-  sprintf(texto, "Content of pointer pc: %d\n\n", *pc); // 11
-  Serial.println(texto);
-
-  *pc = 2;
-  sprintf(texto, "Address of c: %p\n", &c);
-  Serial.println(texto);
-
-  sprintf(texto, "Value of c: %d\n\n", c); // 2
-  Serial.println(texto);
+void function(int *a, int b)
+{
+  *a = b + 5;
+  b = 0;
 }

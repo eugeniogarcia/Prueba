933ad8540895fd55a3e8305d8f8105be92634779
Eugenio Garcia San Martin <egsmartin@gmail.com>
2024-11-23 08:19:42 +0100

    draft

---
+++ C/src/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    struct Point
+    {
+        float X_coord;
+        float Y_coord;
+    };
+    struct Point array_point[10];
+    struct Point mean_point;
+    Point array_point1[10];
+    Point mean_point1;
+
+    printf("Hello World\n");
+    char a = 0x61;
+    char b = 97;
+    char c = 'a';
+    int i = a;
+    int j=97;
+    char d=j;
+    return 0;
+}
--- LabEx1/src/main.cpp
+++ LabEx1/src/main.cpp
@@ -1,25 +1,35 @@
 #include <Arduino.h>
-#define LEN 10
+#define LEN 11 // cabio1
 
-int numbers [LEN];
+int numbers[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11};
+char numbers2[] = {0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
 char text[50];
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  //Array initialization
+  /*
+  // Array initialization
   int i;
-  for (i=0; i<LEN;i++){
-    numbers [i] = i+1;
+  for (i = 0; i < LEN; i++)
+  {
+    numbers[i] = i + 1;
   }
+  */
 }
 
-void loop() {
+void loop()
+{
   // Factorial computation
   int factorial;
-  int F=1;
+  int F = 1;
+  int F2 = 1;
   int i;
-  for (i=0;i<LEN;i++){
-    F = F*numbers[i];
+  for (i = 0; i < LEN; i++)
+  {
+    F = F * numbers[i];
+    //F2 = F2 * numbers2[i];
+    F2 = F2 * static_cast<int>(numbers2[i]);
   }
   factorial = F;
   sprintf(text, "Factorial = %d", factorial);
@@ -27,6 +37,7 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }

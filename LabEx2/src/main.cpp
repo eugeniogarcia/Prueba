6a2dc038bd7cb2904ae0fc091d20b174c3727a8e
Eugenio Garcia San Martin <egsmartin@gmail.com>
2024-11-23 21:44:33 +0100

    .

--- LabEx2/src/main.cpp
+++ LabEx2/src/main.cpp
@@ -1,25 +1,44 @@
 #include <Arduino.h>
 
 #define LEN 10
-int array_input[LEN] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int array_output[LEN] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float X_values[LEN] = {3.1, 3.2, 4.0, 1.0, 3.0, 5.0, -7.0, -8.0, 9.0, 1.0};
+float Y_values[LEN] = {-0.2, 3.5, 7.8, -2, 2.3, 3.0, 3.2, 4.0, 1.0, -3.0};
+struct Point
+{
+  float X_coord;
+  float Y_coord;
+};
+
+struct Point array_point[LEN];
+struct Point mean_point;
 
 void setup()
 {
+  int i;
   Serial.begin(9600);
-  Serial.println("Pointers and Arrays\n");
+  Serial.println("Pointers and structs\n");
+  for (i = 0; i < 10; i++)
+  {
+    array_point[i].X_coord = X_values[i];
+    array_point[i].Y_coord = Y_values[i];
+  }
 }
 
 void loop()
 {
-  int *p_ini, *p_end;
+  struct Point *p_struct;
+
   int i;
-  p_ini = array_input;
-  p_end = array_output + 9;
+  mean_point.X_coord = 0;
+  mean_point.Y_coord = 0;
+  p_struct = &(array_point[0]); // same to p_struct = array_point
   for (i = 0; i < 10; i++)
   {
-    *p_end = *p_ini;
-    p_ini++;
-    p_end--;
+    mean_point.X_coord += p_struct->X_coord;
+    mean_point.Y_coord += p_struct->Y_coord;
+    p_struct++;
   }
+  mean_point.X_coord /= LEN;
+  mean_point.Y_coord /= LEN;
+  Serial.println("End\n");
 }
